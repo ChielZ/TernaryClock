1ee3278d55421d16b6027f1fb66397ff63eba62b
Chiel Zwinkels <chiel.zwinkels@gmail.com>
2026-04-03 22:12:50 +0200

    Initial Commit

---
+++ TernaryClock/TernaryClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TernaryClockApp.swift
+//  TernaryClock
+//
+//  Created by Chiel Zwinkels on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TernaryClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
